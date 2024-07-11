2cb94ca2d3c39f6c658b191854d7150d1b978b87
anar1997 <anar.quliyev.00@gmail.com>
2024-07-11 18:17:24 +0400

    cover-32

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,7 +4,8 @@ import { initialPerfumes } from '../InitialPerfumes';
 import { Input, Pagination } from 'antd';
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { RiTiktokLine } from 'react-icons/ri';
-import coverImage from "../../images/cover-26.avif";
+import coverImage from "../../images/cover-31.avif";
+import coverPerfume from "../../images/cover-32.avif";
 import style from "./style.module.css";
 
 
@@ -61,7 +62,14 @@ const Content = () => {
                     />
                 </div>
                 {currentPerfumes.map((perfume) => (
-                    <NavLink key={perfume.id} to={`/${perfume.id}`} className={`flex flex-col items-center w-2/4 h-20 rounded-lg shadow-md brightness-125 contrast-200hover:-translate-y-1 hover:cursor-pointer hover:scale-110 hover:bg-slate-900 duration-300  bg-emerald-800 mb-5 mx-auto`}>
+                    <NavLink key={perfume.id} to={`/${perfume.id}`} className={`${style.perfumeNav} flex flex-col items-center w-2/4 h-20 rounded-full shadow-md brightness-125 contrast-200hover:-translate-y-1 hover:cursor-pointer hover:scale-110 hover:bg-slate-900 duration-300 mb-5 mx-auto`}
+                        style={{
+                            backgroundImage: `url(${coverPerfume})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                    >
                         <h2 className={`my-auto italic text-xl text-orange-300`}>{perfume.name}</h2>
                         <p className={`my-auto text-neutral-300`}>{perfume.price}</p>
                     </NavLink>
