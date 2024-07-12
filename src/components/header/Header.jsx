b631f3dc71caae1e922c8f04071dcef4c99ceab4
anar1997 <anar.quliyev.00@gmail.com>
2024-07-13 01:35:53 +0400

    headerouder

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import odeur from '../../images/odeur2.jpg'
-import style from "./style.module.css";
-
 
 
 const Header = () => {
   return (
-    <div className={`bg-black ${style.header} w-full shadow-2xl`}>
+    <div className={`bg-black h-80 w-full shadow-2xl`}>
       <div className={`relative h-full`}>
         <img 
           src={odeur}
-          className={`${style.img} w-full`}
+          className={`h-80 w-full`}
         />
       </div>
     </div>
